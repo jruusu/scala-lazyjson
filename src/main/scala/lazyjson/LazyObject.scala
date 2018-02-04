cea20f6b2f201e8d768ae441b0b52567c0dc7e30
Jan Ruusuvuori <jruusu@iki.fi>
2018-02-04 14:37:35 +0200

    LazyObject now behaves like a Map

--- src/main/scala/lazyjson/LazyObject.scala
+++ src/main/scala/lazyjson/LazyObject.scala
@@ -1,9 +1,31 @@
 package lazyjson
 
-class LazyObject(underlying: me.doubledutch.lazyjson.LazyObject) extends LazyElement {
-  def get(key: String): Any = wrap(underlying.get(key))
+class LazyObject(underlying: me.doubledutch.lazyjson.LazyObject) extends Map[String, Any] with LazyElement {
+  override def +[V1 >: Any](kv: (String, V1)): Map[String, V1] = {
+    underlying.put(kv._1, kv._2)
+    this
+  }
 
-  def opt(key: String): Option[Any] = Option(underlying.opt(key)).map(wrap)
+  override def get(key: String): Option[Any] = Option(underlying.opt(key)).map(wrap)
+
+  override def iterator: Iterator[(String, Any)] = new Iterator[(String, Any)] {
+
+    import scala.collection.JavaConverters._
+
+    private val keys = underlying.keys().asScala
+
+    override def hasNext: Boolean = keys.hasNext
+
+    override def next(): (String, Any) = {
+      val key = keys.next()
+      (key, wrap(underlying.get(key)))
+    }
+  }
+
+  override def -(key: String): Map[String, Any] = {
+    underlying.remove(key)
+    this
+  }
 }
 
 object LazyObject {
--- src/test/scala/lazyjson/LazyObjectSpec.scala
+++ src/test/scala/lazyjson/LazyObjectSpec.scala
@@ -7,29 +7,29 @@ class LazyObjectSpec extends FlatSpec with Matchers {
   private val str = """{"foo":9,"bar":true,"baz":3.1415,"sval":"hello world","fval":false,"nval":null,"aval":[2,2,4],"oval":{"foo":42},"eval":"\n"}"""
   private val obj = LazyObject(str)
 
-  "get" should "chooch" in {
-    obj.get("foo") shouldEqual 9L
-    obj.get("bar") shouldEqual true
-    obj.get("baz") shouldEqual 3.1415
-    obj.get("sval") shouldEqual "hello world"
-    obj.get("fval") shouldEqual false
-    obj.get("nval") shouldEqual LazyObject.NULL
-    obj.get("aval") shouldBe a[LazyArray]
-    obj.get("oval") shouldBe a[LazyObject]
-    obj.get("eval") shouldEqual "\n"
-    assertThrows[me.doubledutch.lazyjson.LazyException] { obj.get("notthere") }
+  "apply" should "chooch" in {
+    obj("foo") shouldEqual 9L
+    obj("bar") shouldEqual true
+    obj("baz") shouldEqual 3.1415
+    obj("sval") shouldEqual "hello world"
+    obj("fval") shouldEqual false
+    obj("nval") shouldEqual LazyObject.NULL
+    obj("aval") shouldBe a[LazyArray]
+    obj("oval") shouldBe a[LazyObject]
+    obj("eval") shouldEqual "\n"
+    assertThrows[NoSuchElementException] { obj("notthere") }
   }
 
-  "opt" should "chooch" in {
-    obj.opt("foo") shouldEqual Some(9L)
-    obj.opt("bar") shouldEqual Some(true)
-    obj.opt("baz") shouldEqual Some(3.1415)
-    obj.opt("sval") shouldEqual Some("hello world")
-    obj.opt("fval") shouldEqual Some(false)
-    obj.opt("nval") shouldEqual Some(LazyObject.NULL)
-    obj.opt("aval").value shouldBe a[LazyArray]
-    obj.opt("oval").value shouldBe a[LazyObject]
-    obj.opt("eval") shouldEqual Some("\n")
-    obj.opt("notthere") shouldEqual None
+  "get" should "chooch" in {
+    obj.get("foo") shouldEqual Some(9L)
+    obj.get("bar") shouldEqual Some(true)
+    obj.get("baz") shouldEqual Some(3.1415)
+    obj.get("sval") shouldEqual Some("hello world")
+    obj.get("fval") shouldEqual Some(false)
+    obj.get("nval") shouldEqual Some(LazyObject.NULL)
+    obj.get("aval").value shouldBe a[LazyArray]
+    obj.get("oval").value shouldBe a[LazyObject]
+    obj.get("eval") shouldEqual Some("\n")
+    obj.get("notthere") shouldEqual None
   }
 }
